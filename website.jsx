516e3bd1976baf3b0bce26f60c6f876e598268a9
Jeff Hykin <jeff.hykin@gmail.com>
2021-10-23 19:35:26 -0500

    add data

--- website.jsx
+++ website.jsx
@@ -1,3 +1,17 @@
+// add CSS before everything else
+require("css-baseline/css/3")
+require("./global.scss")
+
+document.body = <div class="centered row">
+    <div class="column centered">
+        <input type="datetime-local" onInput={e=>console.log(e)} />
+    </div>
+    <div style="min-width: 2rem;"></div>
+    <div class="column centered">
+        <input type="number" onInput={e=>console.log(e)} />
+    </div>
+</div>
+
 document.body.style = `
     display: flex; 
     align-items: center; 
