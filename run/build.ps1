449220988f3abb19ba83477622cd165c7faed1b4
Jeff Hykin <jeff.hykin@gmail.com>
2025-03-13 12:28:58 -0500

    fix build

--- run/build.ps1
+++ run/build.ps1
@@ -6,8 +6,8 @@ echo --% >/dev/null;: ' | out-null
 # for not-Windows operating systems
 #
 rm -rf ./docs
-deno run -A 'https://esm.sh/gh/jeff-hykin/bite@e070c4af1e/vite/bin/vite.js' build ./main --outDir ../docs
-deno run -A https://deno.land/x/html_bundle/main/html-bundle.js --inplace ./docs/index.html 
+deno run -A 'https://esm.sh/gh/jeff-hykin/bite@a73701e/vite/bin/vite.js' build ./main --outDir ../docs --emptyOutDir
+deno run -A https://esm.sh/gh/jeff-hykin/html-bundle@0.0.3.0/main/html-bundle.js --inplace ./docs/index.html
 
 exit #>
 
@@ -15,5 +15,5 @@ exit #>
 # for windows (powershell)
 #
 rm -rf ./docs
-deno run -A 'https://esm.sh/gh/jeff-hykin/bite@e070c4af1e/vite/bin/vite.js' build ./main --outDir ../docs
-deno run -A https://deno.land/x/html_bundle/main/html-bundle.js --inplace ./docs/index.html 
+deno run -A 'https://esm.sh/gh/jeff-hykin/bite@5.4.10.2/vite/bin/vite.js' build ./main --outDir ../docs
+deno run -A https://esm.sh/gh/jeff-hykin/html-bundle@0.0.3.0/main/html-bundle.js --inplace ./docs/index.html
